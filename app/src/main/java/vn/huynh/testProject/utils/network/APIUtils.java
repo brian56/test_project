344798353f629d17fb345a7654324083bcd173cb
duong <huynhdq91@gmail.com>
2019-04-01 23:45:54 +0700

    first commit

---
+++ app/src/main/java/vn/huynh/testProject/utils/network/APIUtils.java
@@ -0,0 +1,13 @@
+package vn.huynh.testProject.utils.network;
+
+/**
+ * Created by duong on 3/28/2019.
+ */
+
+public class APIUtils {
+    public static final String BASE_URL = "https://raw.githubusercontent.com";
+
+    public static KeyWordService getKeyWordService() {
+        return RetrofitClient.getClient(BASE_URL).create(KeyWordService.class);
+    }
+}
